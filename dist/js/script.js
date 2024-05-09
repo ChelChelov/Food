f35ab6374534cb5c3fb020b7b3bf7538f5f3d7f2
Oleg Sazonov <sazonov1994@gmail.com>
2024-05-09 18:48:57 +0300

    add timer(js)

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,9 +7,12 @@ var __webpack_exports__ = {};
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  //Tabs
   const tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent'),
     tabsParent = document.querySelector('.tabheader__items');
+
+  // Function to hide <div class="tabcontent"> and <div class="tabheader__item">
   function hideTabContent() {
     tabsContent.forEach(item => {
       item.classList.add('hide');
@@ -19,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
       item.classList.remove('tabheader__item_active');
     });
   }
+  // Function to show <div class="tabcontent"> and <div class="tabheader__item"> by it's index
   function showTabContent(i = 0) {
     tabsContent[i].classList.add('show', 'fade');
     tabsContent[i].classList.remove('hide');
@@ -26,6 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   hideTabContent();
   showTabContent();
+
+  //Delegated eventListener for <div class="tabheader__item"> to show choosen one tab and tabcontent
   tabsParent.addEventListener('click', e => {
     const target = e.target;
     if (target && target.classList.contains('tabheader__item')) {
@@ -37,6 +43,51 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  //Timer
+
+  const deadLine = '2024-05-21';
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor(t / (1000 * 60 * 60) % 24),
+      minutes = Math.floor(t / 1000 / 60 % 60),
+      seconds = Math.floor(t / 1000 % 60);
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock('.timer', deadLine);
 });
 /******/ })()
 ;
