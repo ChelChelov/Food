2aaa4551f1d77241d6edf2a132eb3148f768479b
Oleg Sazonov <sazonov1994@gmail.com>
2024-05-22 18:27:19 +0300

    update Menu-item class

--- dist/js/script.js
+++ dist/js/script.js
@@ -135,16 +135,17 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   window.addEventListener('scroll', showModalByScroll);
 
-  //Menu-item classes
+  //Menu-item class
 
   class MenuItem {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
       this.parent = document.querySelector(parentSelector);
+      this.classes = classes;
       this.transfer = 40;
       this.changeToUAH();
     }
@@ -153,16 +154,20 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     render() {
       const item = document.createElement('div');
+      if (this.classes.length === 0) {
+        this.item = 'menu__item';
+        item.classList.add(this.item);
+      } else {
+        this.classes.forEach(className => item.classList.add(className));
+      }
       item.innerHTML = `
-				<div class="menu__item">
-					<img src=${this.src} alt=${this.alt}>
-					<h3 class="menu__item-subtitle">${this.title}</h3>
-					<div class="menu__item-descr">${this.descr}</div>
-					<div class="menu__item-divider"></div>
-					<div class="menu__item-price">
-						<div class="menu__item-cost">Цена:</div>
-						<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-					</div>
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
 				</div>
 			`;
       this.parent.append(item);
